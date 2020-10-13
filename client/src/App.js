c3537c86a0612d3096d3f7622800768f92c7df12
MorganWilliamson <mwilliamson7695@gmail.com>
2020-10-12 20:03:43 -0500

    Working on Movie.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import SavedList from './Movies/SavedList';
+import { Route, Switch } from "react-router-dom";
+import Movie from "./Movies/Movie";
+import MovieList from "./Movies/MovieList";
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -11,9 +14,10 @@ export default function App () {
     const getMovies = () => {
       axios
         .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
-        .then(response => {
+        .then((response) => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
